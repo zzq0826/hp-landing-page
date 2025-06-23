ec169b0fc01dfb69ac771e49c3c01e59a15d17f6
zzq0826 <770166635@qq.com>
2025-06-23 09:34:46 +0800

    adjust

--- src/pages/home/Header.tsx
+++ src/pages/home/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => (
-  <header className="fixed top-0 left-0 right-0 z-10 bg-black bg-opacity-50 backdrop-blur-md">
+  <header className="fixed top-0 left-0 right-0 z-10  bg-opacity-50 backdrop-blur-md border-b border-gray-700 bg-[#090b13]">
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex h-16 items-center justify-between">
         <div className="flex items-center space-x-2">
