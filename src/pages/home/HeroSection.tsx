cd4c46259834394076585c984cccfc42875a3554
zzq0826 <770166635@qq.com>
2025-06-25 08:27:07 +0800

    adjust style

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -87,7 +87,7 @@ const HeroSection = () => {
                     <div>
                       <div className="mt-4 flex">
                         <input
-                          className="flex-grow rounded-l-md bg-gray-900 px-4 py-2 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                          className="flex-grow rounded-l-md bg-gray-900 px-4 py-2 text-white focus:outline-none "
                           name="EMAIL"
                           onChange={handleChangeEmail}
                           onKeyDown={(e) => handleEnter(e, subscribe)}
